995fcdae15591d7da904e283153e14841a874fd2
hhiga <60108594+hhiga@users.noreply.github.com>
2020-06-19 15:08:12 -0300

    Add the login in the main page Add bootsrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService, User } from './core/services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularApp';
+  currentUser: User;
+
+  constructor(private accountService: AccountService) {
+    this.accountService.user$.subscribe(user => this.currentUser = user);
+  }
+
+  ngOnInit(): void {
+  }
+
 }
--- src/app/core/services/account.service.ts
+++ src/app/core/services/account.service.ts
@@ -2,12 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { UserManager, UserManagerSettings, User } from 'oidc-client';
+
+export { User };
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private manager: UserManager;
   private user: User | null;
+  private _user = new BehaviorSubject<User>(null);
 
   constructor(private httpClient: HttpClient) {
     this.manager = new UserManager(
@@ -20,7 +24,10 @@ export class AccountService {
         scope: 'WeatherApi openid profile',
         response_mode: 'query'
       });
-    this.manager.getUser().then(user => this.user = user);
+    this.manager.getUser().then(user => {
+      this.user = user;
+      this._user.next(user);
+    });
    }
 
    public getUser(): Promise<User> {
@@ -31,8 +38,9 @@ export class AccountService {
      return this.manager.signinRedirect();
    }
 
-   public renetToken(): Promise<User> {
-     return this.manager.signinSilent();
+   public async renewToken(): Promise<void> {
+     this.user = await this.manager.signinSilent();
+     this._user.next(this.user);
    }
 
    public logout(): Promise<void> {
@@ -41,10 +49,15 @@ export class AccountService {
 
    public async completeAuthentication() {
      this.user = await this.manager.signinRedirectCallback();
+     this._user.next(this.user);
    }
 
    public isAuthenticated(): boolean {
      return this.user != null && !this.user.expired;
+   }
+
+   get user$(): Observable<User> {
+     return this._user.asObservable();
    }
 }
 
